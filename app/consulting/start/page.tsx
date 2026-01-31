284b20465a79b16c07d3babc1e02481e629fcc9c
eiichi <emorimoto@msouken.net>
2026-01-31 19:46:37 +0900

    fix: 相談開始前のファイル添付を無効化
    問題:
    - 相談が開始されていない状態でもファイルを添付できてしまう
    - currentSession がnullの状態でファイルアップロード可能
    
    修正内容:
    1. ContextPanel.tsx
       - Props追加: disabled?: boolean
       - disabled=true時の動作:
         - ドラッグ&ドロップ無効化
         - クリック選択無効化
         - 視覚的フィードバック: opacity-50, cursor-not-allowed
         - メッセージ表示: "相談を開始してください"
       - イベントハンドラに早期リターン追加
       - input要素にdisabled属性追加
    
    2. start/page.tsx
       - ContextPanelに disabled={!currentSession} を追加
       - currentSessionがnullの場合、ファイル添付を無効化
    
    UI変更:
    - 無効時: AlertCircleアイコン + 案内メッセージ表示
    - 有効時: 従来通りのドロップゾーン表示

--- app/consulting/start/page.tsx
+++ app/consulting/start/page.tsx
@@ -475,6 +475,7 @@ export default function ConsultingPage() {
                 }
               }}
               onRemoveAttachment={handleRemoveAttachment}
+              disabled={!currentSession}
             />
           </div>
         </div>
